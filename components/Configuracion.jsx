24845e14c1b79e8ddb3c6ffe49ce01f5896001a1
Miguel Gordillo <elloquius@gmail.com>
2025-10-27 19:19:42 -0400

    Conflicto resuelto

--- components/Configuracion.jsx
+++ components/Configuracion.jsx
@@ -1,8 +1,8 @@
 const Configuracion = () => {
   return (
     <div className="p-4">
-      <h2 className="text-xl font-bold mb-2">Configuracion</h2>
-      <p>Aquí iría la información de Configuracion...</p>
+      <h2 className="text-xl font-bold mb-2">Configuración</h2>
+      <p>Aquí iría la información de Configuración...</p>
     </div>
   );
 };

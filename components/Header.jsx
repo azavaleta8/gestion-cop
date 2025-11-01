78bad672ee2afe17602e6355b3053286e618da4e
Sergio Carrillo <14-11315@usb.ve>
2025-11-01 01:57:30 -0400

    fix label Cargos -> Rangos

--- components/Header.jsx
+++ components/Header.jsx
@@ -39,7 +39,7 @@ export default function Header() {
     { href: "/home/guardias", label: "Guardias" },
     { href: "/home/trabajadores", label: "Funcionarios" },
     { href: "/home/localizaciones", label: "Servicios" },
-    { href: "/home/roles", label: "Cargos" },
+    { href: "/home/roles", label: "Rangos" },
   ];
 
   const handleLogout = async () => {
